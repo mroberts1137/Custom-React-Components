ab37b69f17c8435961aeb4aab877141dea1fc0d2
mroberts1137 <mroberts1137@gmail.com>
2024-01-04 19:50:53 -0500

    LoginForm

---
+++ LoginForm.js
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+
+const initialFormValues = {
+  username: '',
+  email: '',
+  password: ''
+};
+
+const LoginForm = () => {
+  const [formValues, updateFormValues] = useState(initialFormValues);
+  const { username, email, password } = formValues;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formValues);
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    updateFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <input
+          value={username}
+          onChange={handleChange}
+          name="username"
+          placeholder="Enter your username"
+        />
+      </div>
+      <div>
+        <input
+          value={email}
+          onChange={handleChange}
+          name="email"
+          placeholder="Enter your email"
+        />
+      </div>
+      <div>
+        <input
+          value={password}
+          onChange={handleChange}
+          name="password"
+          placeholder="Enter your password"
+        />
+      </div>
+      {username && email && password && <button type="submit">Submit</button>}
+    </form>
+  );
+};
+
+export default LoginForm;
